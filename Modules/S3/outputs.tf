2de1b92e709102f7af4e0e016004a04adf39a7e1
Lilantha Rajapaksha <lilantha.rajapaksha@gmail.com>
2023-06-23 17:39:43 +0530

    Update outputs.tf

--- Modules/S3/outputs.tf
+++ Modules/S3/outputs.tf
@@ -1,11 +1,16 @@
-# output "id" {
-#   value = aws_s3_bucket.s3.id
-# }
+output "id" {
+  value = aws_s3_bucket.s3_web.id
+}
 
-# output "arn" {
-#   value = aws_s3_bucket.s3.arn
-# }
+output "arn" {
+  value = aws_s3_bucket.s3_web.arn
+}
 
-# output "bucket_domain_name" {
-#   value = aws_s3_bucket.s3.bucket_domain_name
-# }
+output "bucket_domain_name" {
+  value = aws_s3_bucket.s3_web.bucket_domain_name
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.s3_web_distribution.domain_name
+}
+
