a82743fb76b793334cfda4c022dbfdec40cf4722
Lilantha Rajapaksha <lilantha.rajapaksha@gmail.com>
2023-06-30 19:35:04 +0530

    Move files to prod

---
+++ Env/Prod/backend.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-workshop-remote-state-s3"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "terraform-workshop-remote-state-s3"
+  }
+}
