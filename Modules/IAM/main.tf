5df796fe809efba846e2c3369e52a5f61632cbd6
Lilantha Rajapaksha <lilantha.rajapaksha@gmail.com>
2023-07-01 11:21:59 +0530

    Revert "Added Codepipeline Role and Policy"
    This reverts commit 93ed0b321b403e812cafaf373e4f21c0c613f9f9.

--- Modules/IAM/main.tf
+++ Modules/IAM/main.tf
@@ -2,7 +2,6 @@
   AWS IAM for different resources
 ============================================*/
 
-### ECS Task Excecution Role and Policies
 # ECS Task Excecution Role
 resource "aws_iam_role" "ecs_task_excecution_role" {
   count              = var.create_ecs_role == true ? 1 : 0
@@ -38,18 +37,6 @@ EOF
   }
 }
 
-# Task Excecution Role Policies Attachment
-resource "aws_iam_role_policy_attachment" "ecs_task_excecution_role_policy_attachment" {
-  count      = length(aws_iam_role.ecs_task_excecution_role) > 0 ? 1 : 0
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_task_excecution_role[0].name
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-### ECS Task Role and Policies
 # ECS Task Role
 resource "aws_iam_role" "ecs_task_role" {
   count              = var.create_ecs_role == true ? 1 : 0
@@ -85,43 +72,9 @@ EOF
   }
 }
 
-# ECS Task Role Policy
-resource "aws_iam_policy" "policy_for_ecs_task_role" {
-  count       = var.create_ecs_role == true ? 1 : 0
-  name        = "${var.service.resource_name_prefix}-${var.name_ecs_task_role}-policy"
-  description = var.description
-  policy      = data.aws_iam_policy_document.role_policy_ecs_task_role.json
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name        = "${var.service.resource_name_prefix}-${var.name_ecs_task_role}-policy"
-    Description = var.description
-    Service     = var.service.app_name
-    Environment = var.service.app_environment
-    Version     = var.service.app_version
-    User        = var.service.user
-    Terraform   = true
-  }
-}
-
-# ECS Task Role Policies Attachments
-resource "aws_iam_role_policy_attachment" "ecs_task_role_policy_attachment" {
-  count      = var.create_ecs_role == true ? 1 : 0
-  policy_arn = aws_iam_policy.policy_for_ecs_task_role[0].arn
-  role       = aws_iam_role.ecs_task_role[0].name
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-### Codepipeline Role and Policies
-# Codepipeline Role
-resource "aws_iam_role" "codepipeline_role" {
-  count              = var.create_codepipeline_role == true ? 1 : 0
+# Devops Role
+resource "aws_iam_role" "devops_role" {
+  count              = var.create_codebuild_role == true ? 1 : 0
   name               = "${var.service.resource_name_prefix}-${var.name}"
   assume_role_policy = <<EOF
 {
@@ -131,6 +84,8 @@ resource "aws_iam_role" "codepipeline_role" {
       "Effect": "Allow",
       "Principal": {
         "Service": [
+          "codebuild.amazonaws.com",
+          "codedeploy.amazonaws.com",
           "codepipeline.amazonaws.com"
         ]
       },
@@ -155,111 +110,6 @@ EOF
   }
 }
 
-# Codepipeline Role Policies
-resource "aws_iam_policy" "codepipeline_role_policy" {
-  count       = var.create_codepipeline_policy == true ? 1 : 0
-  name        = "${var.service.resource_name_prefix}-${var.name}-policy"
-  description = var.description
-  policy      = data.aws_iam_policy_document.role_policy_codepipeline_role.json
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name        = "${var.service.resource_name_prefix}-${var.name}-policy"
-    Description = var.description
-    Service     = var.service.app_name
-    Environment = var.service.app_environment
-    Version     = var.service.app_version
-    User        = var.service.user
-    Terraform   = true
-  }
-}
-
-# Codepipeline Role Policies Attachment
-resource "aws_iam_role_policy_attachment" "codedeploy_role_policy_attachment2" {
-  count      = var.create_codepipeline_policy == true ? 1 : 0
-  policy_arn = aws_iam_policy.codepipeline_role_policy[0].arn
-  role       = var.attach_to
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-
-### Codebuild Role and Policies
-# Codebuild Role
-resource "aws_iam_role" "codebuild_role" {
-  count              = var.create_codebuild_role == true ? 1 : 0
-  name               = "${var.service.resource_name_prefix}-${var.name}"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-          "codebuild.amazonaws.com"
-        ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name        = "${var.service.resource_name_prefix}-${var.name}"
-    Description = var.description
-    Service     = var.service.app_name
-    Environment = var.service.app_environment
-    Version     = var.service.app_version
-    User        = var.service.user
-    Terraform   = true
-  }
-}
-
-# Codebuild Role Policies
-resource "aws_iam_policy" "codebuild_role_policy" {
-  count       = var.create_codebuild_policy == true ? 1 : 0
-  name        = "${var.service.resource_name_prefix}-${var.name}-policy"
-  description = var.description
-  policy      = data.aws_iam_policy_document.role_policy_codebuild_role.json
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Name        = "${var.service.resource_name_prefix}-${var.name}-policy"
-    Description = var.description
-    Service     = var.service.app_name
-    Environment = var.service.app_environment
-    Version     = var.service.app_version
-    User        = var.service.user
-    Terraform   = true
-  }
-}
-
-# Codebuild Role Policies Attachment
-resource "aws_iam_role_policy_attachment" "codebuild_role_policy_attachment" {
-  count      = var.create_codebuild_policy == true ? 1 : 0
-  policy_arn = aws_iam_policy.codebuild_role_policy[0].arn
-  role       = var.attach_to
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-### Codedeploy Role and Policies
 # Codedeploy Role
 resource "aws_iam_role" "codedeploy_role" {
   count              = var.create_codedeploy_role == true ? 1 : 0
@@ -291,11 +141,269 @@ EOF
   }
 }
 
-# Codedeploy Role Policies Attachment
-resource "aws_iam_role_policy_attachment" "codedeploy_role_policy_attachment" {
+# IAM Policies
+resource "aws_iam_policy" "policy_for_role" {
+  count       = var.create_codebuild_policy == true ? 1 : 0
+  name        = "${var.service.resource_name_prefix}-${var.name}-policy"
+  description = var.description
+  policy      = data.aws_iam_policy_document.role_policy_devops_role.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name        = "${var.service.resource_name_prefix}-${var.name}-policy"
+    Description = var.description
+    Service     = var.service.app_name
+    Environment = var.service.app_environment
+    Version     = var.service.app_version
+    User        = var.service.user
+    Terraform   = true
+  }
+}
+
+# Policy for ECS Task Role
+resource "aws_iam_policy" "policy_for_ecs_task_role" {
+  count       = var.create_ecs_role == true ? 1 : 0
+  name        = "${var.service.resource_name_prefix}-${var.name_ecs_task_role}-policy"
+  description = var.description
+  policy      = data.aws_iam_policy_document.role_policy_ecs_task_role.json
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name        = "${var.service.resource_name_prefix}-${var.name_ecs_task_role}-policy"
+    Description = var.description
+    Service     = var.service.app_name
+    Environment = var.service.app_environment
+    Version     = var.service.app_version
+    User        = var.service.user
+    Terraform   = true
+  }
+}
+
+# IAM Policies Attachments
+resource "aws_iam_role_policy_attachment" "ecs_attachment" {
+  count      = var.create_ecs_role == true ? 1 : 0
+  policy_arn = aws_iam_policy.policy_for_ecs_task_role[0].arn
+  role       = aws_iam_role.ecs_task_role[0].name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "attachment" {
+  count      = length(aws_iam_role.ecs_task_excecution_role) > 0 ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_excecution_role[0].name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "attachment2" {
+  count      = var.create_codebuild_policy == true ? 1 : 0
+  policy_arn = aws_iam_policy.policy_for_role[0].arn
+  role       = var.attach_to
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_attachment" {
   count      = var.create_codedeploy_role == true ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
   role       = aws_iam_role.codedeploy_role[0].name
 }
 
+# IAM Policy Documents
+data "aws_iam_policy_document" "role_policy_devops_role" {
+  statement {
+    sid    = "AllowS3Actions"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketAcl",
+      "s3:List*"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowCodebuildActions"
+    effect = "Allow"
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+      "codebuild:BatchGetBuildBatches",
+      "codebuild:StartBuildBatch",
+      "codebuild:StopBuild"
+    ]
+    resources = var.code_build_projects
+  }
+  statement {
+    sid    = "AllowCodebuildList"
+    effect = "Allow"
+    actions = [
+      "codebuild:ListBuilds"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowCodeDeployConfigs"
+    effect = "Allow"
+    actions = [
+      "codedeploy:GetDeploymentConfig",
+      "codedeploy:CreateDeploymentConfig",
+      "codedeploy:CreateDeploymentGroup",
+      "codedeploy:GetDeploymentTarget",
+      "codedeploy:StopDeployment",
+      "codedeploy:ListApplications",
+      "codedeploy:ListDeploymentConfigs",
+      "codedeploy:ListDeploymentGroups",
+      "codedeploy:ListDeployments"
 
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowECRActions"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:BatchGetImage",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = concat(var.ecr_repositories, ["arn:aws:ecr:ap-southeast-2:642801335081:repository/node-18-alpine"])
+  }
+  statement {
+    sid    = "AllowECRAuthorization"
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowCECSServiceActions"
+    effect = "Allow"
+    actions = [
+      "ecs:ListServices",
+      "ecs:ListTasks",
+      "ecs:DescribeServices",
+      "ecs:DescribeTasks",
+      "ecs:DescribeTaskDefinition",
+      "ecs:DescribeTaskSets",
+      "ecs:DeleteTaskSet",
+      "ecs:DeregisterContainerInstance",
+      "ecs:CreateTaskSet",
+      "ecs:UpdateCapacityProvider",
+      "ecs:PutClusterCapacityProviders",
+      "ecs:UpdateServicePrimaryTaskSet",
+      "ecs:RegisterTaskDefinition",
+      "ecs:RunTask",
+      "ecs:StartTask",
+      "ecs:StopTask",
+      "ecs:UpdateService",
+      "ecs:UpdateCluster",
+      "ecs:UpdateTaskSet"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowIAMPassRole"
+    effect = "Allow"
+    actions = [
+      "iam:PassRole"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowDescribeSecurityGroups"
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeSecurityGroups",
+      "ec2:CreateNetworkInterface",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:AssignPrivateIpAddresses"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowVpcActions"
+    effect = "Allow"
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeVpcs",
+      "ec2:CreateNetworkInterfacePermission",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupIngress",
+      "ec2:CreateTags",
+      "ec2:DescribeDhcpOptions"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowCloudWatchActions"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowConnectionActions"
+    effect = "Allow"
+    actions = [
+      "codestar-connections:UseConnection"
+    ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "role_policy_ecs_task_role" {
+  statement {
+    sid    = "AllowS3Actions"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket"
+    ]
+    resources = var.s3_bucket_assets
+  }
+  statement {
+    sid    = "AllowIAMPassRole"
+    effect = "Allow"
+    actions = [
+      "iam:PassRole"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowRDSdbActions"
+    effect = "Allow"
+    actions = [
+      "rds:*",
+    ]
+    resources = var.database
+  }
+}
